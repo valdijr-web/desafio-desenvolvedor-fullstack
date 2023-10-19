c13f80288e5f6b88022142466e41e9991928c84a
valdijunior <valdi.ads@gmail.com>
2023-10-19 10:19:07 -0300

    tradução datatables

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -64,6 +64,29 @@
                         }
                     }
                 ],
+                "language": {
+                    "sEmptyTable": "Nenhum registro encontrado",
+                    "sInfo": "Mostrando de _START_ a _END_ de _TOTAL_ registros",
+                    "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",
+                    "sInfoFiltered": "(filtrado de _MAX_ registros no total)",
+                    "sInfoPostFix": "",
+                    "sInfoThousands": ".",
+                    "sLengthMenu": "Mostrar _MENU_ registros por página",
+                    "sLoadingRecords": "Carregando...",
+                    "sProcessing": "Processando...",
+                    "sSearch": "Pesquisar:",
+                    "sZeroRecords": "Nenhum registro correspondente encontrado",
+                    "oPaginate": {
+                        "sFirst": "Primeiro",
+                        "sLast": "Último",
+                        "sNext": "Próximo",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending": ": Ative para ordenar a coluna em ordem crescente",
+                        "sSortDescending": ": Ative para ordenar a coluna em ordem decrescente"
+                    }
+                }
             });
         });
 
