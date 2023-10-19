f0db56a685df7e42a95ee6b776d5bfffafe8b096
valdijunior <valdi.ads@gmail.com>
2023-10-19 11:12:25 -0300

    Rotas api pedidos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SolicitaionController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,12 @@ Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::get('users', [UserController::class, 'index'])->name('api.users.index');
 Route::post('users', [UserController::class, 'store'])->name('users.store');
 Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+
+
+Route::delete('solicitations/{user}', [SolicitaionController::class, 'destroy'])->name('solicitations.destroy');
+Route::get('solicitations/{user}', [SolicitaionController::class, 'show'])->name('solicitations.show');
+Route::get('solicitations', [SolicitaionController::class, 'index'])->name('api.solicitations.index');
+Route::get('solicitations', [SolicitaionController::class, 'getUserSolicitations'])->name('api.solicitations.getUserSolicitations');
+Route::post('solicitations', [SolicitaionController::class, 'store'])->name('solicitations.store');
+Route::put('solicitations/{user}', [SolicitaionController::class, 'update'])->name('solicitations.update');
 
