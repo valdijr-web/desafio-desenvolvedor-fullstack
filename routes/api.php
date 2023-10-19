4e7bc42103184fc8249a79a0eae0572165a2a067
valdijunior <valdi.ads@gmail.com>
2023-10-19 08:31:08 -0300

    Routes API

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-    return response()->json([
-        'sucess' => true
-    ]);
-});
+Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::get('users', [UserController::class, 'index'])->name('api.users.index');
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+
